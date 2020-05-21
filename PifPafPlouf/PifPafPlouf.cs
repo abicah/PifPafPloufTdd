896917d85ad8392046b5640716aff9989eabee65
newtom69 <thomas@vuille.fr>
2020-05-21 17:07:13 +0200

    step 5 still ko

--- PifPafPlouf/PifPafPlouf.cs
+++ PifPafPlouf/PifPafPlouf.cs
@@ -6,10 +6,13 @@ namespace PifPafPloufProject
     {
         public string SuitElementFromNumber(int number)
         {
+
             if (number % 3 == 0)
                 return "Pif";
             if (number % 4 == 0)
                 return "Paf";
+            if (number % 12 == 0)
+                return "Plouf";
             return number.ToString();
         }
 
