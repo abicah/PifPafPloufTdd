d7a06baea84b76d90b9995641ba0c0d3df73febc
newtom69 <thomas@vuille.fr>
2020-05-21 17:47:30 +0200

    main program for complet response

--- PifPafPlouf/Program.cs
+++ PifPafPlouf/Program.cs
@@ -6,8 +6,11 @@ namespace PifPafPloufProject
     {
         static void Main(string[] args)
         {
-            PifPafPlouf pifPaf = new PifPafPlouf();
-
+            int minNumber = 1;
+            int maxNumber = 180;
+            PifPafPlouf pifPafplouf = new PifPafPlouf();
+            string suit = pifPafplouf.SuitBetween2Number(minNumber, maxNumber);
+            Console.WriteLine(suit);
         }
     }
 }
