acf40a44ca393637f949d44dcfcc97d91b69d251
abicah <31044489+abicah@users.noreply.github.com>
2024-06-01 18:06:09 +0200

    Update PifPafPlouf.cs
    Factorisation method SuitElementFromNumber
    and SuitBetween2Number

--- PifPafPlouf/PifPafPlouf.cs
+++ PifPafPlouf/PifPafPlouf.cs
@@ -4,24 +4,23 @@ namespace PifPafPloufProject
 {
     public class PifPafPlouf
     {
-        public string SuitElementFromNumber(int number)
-        {
-            if (number % 12 == 0)
-                return "Plouf";
-            if (number % 3 == 0)
-                return "Pif";
-            if (number % 4 == 0)
-                return "Paf";
-
-            return number.ToString();
-        }
-
+       public string SuitElementFromNumber(int number)
+            => (number % 3, number % 4) switch
+            {
+            (0, 0) => "Plouf",
+            (0, _) => "Pif",
+            (_, 0) => "Paf",
+            _ => number.ToString()
+            };
+                
+                
         public string SuitBetween2Number(int minNumber, int maxNumber)
         {
-            string suit = "";
-            for (int currentNumber = minNumber; currentNumber <= maxNumber; currentNumber++)
-                suit += SuitElementFromNumber(currentNumber);
-            return suit;
+            StringBuilder builder = new();
+            foreach (var item in Enumerable.Range(minNumber, maxNumber - minNumber + 1))
+              builder.Append(PifPafPlouf(item));
+            
+            return builder.ToString();
         }
     }
 }
